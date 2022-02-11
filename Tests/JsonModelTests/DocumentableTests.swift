b65f10d375ae77e0786222421b9cddba982e3f5d
Shannon Young <shannon.young@sagebase.org>
2022-02-10 16:01:58 -0800

    Update copyright dates

--- Tests/JsonModelTests/DocumentableTests.swift
+++ Tests/JsonModelTests/DocumentableTests.swift
@@ -2,7 +2,7 @@
 //  DocumentableTests.swift
 //
 //
-//  Copyright © 2020 Sage Bionetworks. All rights reserved.
+//  Copyright © 2020-2022 Sage Bionetworks. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without modification,
 // are permitted provided that the following conditions are met:
--- Tests/JsonModelTests/ResultDataTests.swift
+++ Tests/JsonModelTests/ResultDataTests.swift
@@ -1,7 +1,7 @@
 //
 //  ResultDataTests.swift
 //
-//  Copyright © 2020-2021 Sage Bionetworks. All rights reserved.
+//  Copyright © 2020-2022 Sage Bionetworks. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without modification,
 // are permitted provided that the following conditions are met:
