996d4807c42f0660c62b4cec0f95230dd1341639
Shannon Young <shannon.young@sagebase.org>
2022-06-29 16:15:53 -0700

    Update min versions in the swift package

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,10 +7,10 @@ let package = Package(
     name: "JsonModel",
     platforms: [
         // Add support for all platforms starting from a specific version.
-        .macOS(.v10_15),
-        .iOS(.v11),
-        .watchOS(.v4),
-        .tvOS(.v11)
+        .macOS(.v11),
+        .iOS(.v14),
+        .watchOS(.v6),
+        .tvOS(.v14)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
